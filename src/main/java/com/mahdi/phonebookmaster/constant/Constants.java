3f200869ddce26766afff1e071b22c913cacb973
mahdi <mahdihp.devsc@gmail.com>
2018-10-26 12:57:31 +0330

    Create User Controller.

---
+++ src/main/java/com/mahdi/phonebookmaster/constant/Constants.java
@@ -0,0 +1,7 @@
+package com.mahdi.phonebookmaster.constant;
+
+public class Constants {
+
+    public static final String KEY_USER_CONTROLLER = "user";
+    public static final String KEY_CONTACT_CONTROLLER = "contact";
+}
