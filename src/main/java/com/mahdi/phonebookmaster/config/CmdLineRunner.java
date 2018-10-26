fe16e73c189752004c559360fa12a8b18a0eadec
mahdi <mahdihp.devsc@gmail.com>
2018-10-26 11:29:07 +0330

    Init Application On Startup

--- src/main/java/com/mahdi/phonebookmaster/config/CmdLineRunner.java
+++ src/main/java/com/mahdi/phonebookmaster/config/CmdLineRunner.java
@@ -1,8 +1,10 @@
 package com.mahdi.phonebookmaster.config;
 
+import com.mahdi.phonebookmaster.model.Contact;
 import com.mahdi.phonebookmaster.model.Privilege;
 import com.mahdi.phonebookmaster.model.Role;
 import com.mahdi.phonebookmaster.model.User;
+import com.mahdi.phonebookmaster.repository.ContactRepository;
 import com.mahdi.phonebookmaster.repository.PrivilegeRepository;
 import com.mahdi.phonebookmaster.repository.RoleRepository;
 import com.mahdi.phonebookmaster.repository.UserRepository;
@@ -14,6 +16,8 @@ import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 
 @Component
@@ -22,36 +26,44 @@ public class CmdLineRunner implements CommandLineRunner {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PrivilegeRepository privilegeRepository;
+    private final ContactRepository contactRepository;
 
     @Autowired
-    public CmdLineRunner(UserRepository userRepository, RoleRepository roleRepository, PrivilegeRepository privilegeRepository) {
+    public CmdLineRunner(UserRepository userRepository, RoleRepository roleRepository, PrivilegeRepository privilegeRepository, ContactRepository contactRepository) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.privilegeRepository = privilegeRepository;
+        this.contactRepository = contactRepository;
     }
-
 
     @Override
     public void run(String... args) throws Exception {
         roleInit();
         userInit();
+        contactInit();
 
+    }
+
+    private void contactInit() {
+        Flux<Contact> contactFlux = Flux.just(
+          new Contact("mahdi","hp","88477474","09339466943","mahdihp@gmail.com")
+        );
+        this.contactRepository.deleteAll()
+                .thenMany(contactRepository.saveAll(contactFlux)).blockLast();
     }
 
     private void roleInit() {
         Flux<Privilege> add_edit_delete_adduser = Flux.just(
                 new Privilege("ADD,EDIT,DELETE,ADDUSER")
         );
-//        this.privilegeRepository.deleteAll()
-//                .thenMany(
-                        this.privilegeRepository.saveAll(add_edit_delete_adduser).blockFirst();
+        this.privilegeRepository.deleteAll()
+                .thenReturn(privilegeRepository.saveAll(add_edit_delete_adduser).blockLast());
 
 
-//        List<Privilege> list_admin = Arrays.asList(add_edit_delete_adduser.blockFirst());
+        List<Privilege> list_admin = Arrays.asList(add_edit_delete_adduser.blockFirst());
 //
-//        Flux<Role> adminRole = Flux.just(new Role(list_admin,"Admin"));
-//        this.roleRepository.deleteAll()
-//                .thenMany(this.roleRepository.saveAll(adminRole));
+        Flux<Role> adminRole = Flux.just(new Role(list_admin, "Admin"));
+        this.roleRepository.deleteAll().thenReturn(roleRepository.saveAll(adminRole).blockFirst());
 
         System.out.println("Role Inserted...");
     }
--- src/main/java/com/mahdi/phonebookmaster/model/Contact.java
+++ src/main/java/com/mahdi/phonebookmaster/model/Contact.java
@@ -1,7 +1,9 @@
 package com.mahdi.phonebookmaster.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.ToString;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,6 +14,9 @@ public class Contact {
 
     @Id
     private ObjectId contactId;
+
+    @JsonIgnore
+    @ToString.Exclude
     private User user;
     private String firstName;
     private String lastName;
@@ -24,6 +29,14 @@ public class Contact {
 
     public Contact(User user, String firstName, String lastName, String homePhone, String mobile, String email) {
         this.user = user;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.homePhone = homePhone;
+        this.mobile = mobile;
+        this.email = email;
+    }
+
+    public Contact(String firstName, String lastName, String homePhone, String mobile, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.homePhone = homePhone;
